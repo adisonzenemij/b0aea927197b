e27f39cf690e98a07bf0ce57bef10ef6afff3200
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-17 21:03:55 -0500

    feat: 4fe66588-5b6a-4b80-89b2-ab0bd68192bf

--- src/main/java/code/Engineering.java
+++ src/main/java/code/Engineering.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class B0aea927197bApplication {
+public class Engineering {
 
 	public static void main(String[] args) {
-		SpringApplication.run(B0aea927197bApplication.class, args);
+		SpringApplication.run(Engineering.class, args);
 	}
 
 }
--- src/test/java/code/EngineeringTests.java
+++ src/test/java/code/EngineeringTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class B0aea927197bApplicationTests {
+class EngineeringTests {
 
 	@Test
 	void contextLoads() {
